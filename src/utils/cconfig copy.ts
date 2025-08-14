06057436760bff02fdbbdb07934de8c0ff7c3698
Rashad Collins <63428010+rcollins22@users.noreply.github.com>
2025-08-14 12:18:58 -0400

    Deleted cconfig copy.ts

--- src/utils/cconfig copy.ts
+++
@@ -1,7 +0,0 @@
-export const Config = {
-   ETHERSCAN_API: 'YOURKEYHERE', // Replace with your actual Etherscan API key
-   POLYGONSCAN_API: 'your_polygonscan_api_key_here',
-   OPENAI_API: 'sk-your-openai-api-key-here',
-   BITQUERY_API: 'your_bitquery_bearer_token_here', // Replace with your actual BitQuery bearer token
- };
- 
